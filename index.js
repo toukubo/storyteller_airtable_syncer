03343a21e2ff220b398619dda6f6628e5913baf5
koji toukubo <toukubo@gmail.com>
2019-05-30 22:39:46 +0900

    - [ ] must be cope with async.

--- index.js
+++ index.js
@@ -2,30 +2,38 @@ var syncer = {}
 
 
 
-//should loop if Storyteller Compose is on
-var nounObjects = [
-    {
-        name: "Noun",
-        project: "storyteller",
-        attrs:[
-            {
-                name: "name",
-                type: "string"
-             },
-         ]
-    },{
-        name: "Attr",
-        project: "storyteller",
-        attrs:[
-            {
-                name: "name",
-                type: "string"
-             },
-         ]
-    }
-];
 
 
+
+require('dotenv')
+
+AIRTABLE_BASE_HASH=process.env.AIRTABLE_BASE_HASH
+AIRTABLE_API_KEY=process.env.AIRTABLE_API_KEY
+
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: AIRTABLE_API_KEY}).base(AIRTABLE_BASE_HASH);
+nounObjects = {}
+
+
+new Promise((resolve,reject) => {
+    base('Nouns').select({
+        view: "Grid view"
+    }).firstPage(function(err, records) {
+        if (err) { console.error(err); return; }
+        nounObjects = records
+        console.log("nounObjects in scope : " + nounObjects)
+    
+        records.forEach(function(record) {
+        // logic for record
+         Noun = record
+          console.log('Retrieved', Noun.get('Name'));
+        });
+    });
+    
+})
+
+console.log("nounObjects out of hte scope : " + nounObjects)
 syncer.nounObjects = nounObjects
 
 module.exports = syncer;
