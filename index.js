91ada148266230377a76909800d6a4e93d599330
koji toukubo <toukubo@gmail.com>
2019-06-03 02:26:41 +0900

    should use setup_path from config file tho, now it's copy and pasted from cli and _sync repo.

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var syncer = {}
-
-
-
-
-
-
+require('./setup_path.js')
 require('dotenv')
 
 AIRTABLE_BASE_HASH=process.env.AIRTABLE_BASE_HASH
@@ -15,25 +10,25 @@ var Airtable = require('airtable');
 var base = new Airtable({apiKey: AIRTABLE_API_KEY}).base(AIRTABLE_BASE_HASH);
 nounObjects = {}
 
+module.exports = syncer;
+syncer.exec = function(models,save){
+    models.forEach(function(model){
+        base(model.TABLE_NAME).select({view: "Grid view"}).firstPage(function(err, records) {
+            records.forEach(function(record) {
+                console.log("start syncing...... model : " + model.ID)
 
-new Promise((resolve,reject) => {
-    base('Nouns').select({
-        view: "Grid view"
-    }).firstPage(function(err, records) {
-        if (err) { console.error(err); return; }
-        nounObjects = records
-        console.log("nounObjects in scope : " + nounObjects)
-    
-        records.forEach(function(record) {
-        // logic for record
-         Noun = record
-          console.log('Retrieved', Noun.get('Name'));
+                var object = {}
+                console.log("record.fields : ")
+                console.dir(record.fields)
+                object = record.fields
+                // object.name = record.get('name')
+                // if(model.ID = NOUNS.ID) noun.project = record.get('project')
+                // if(model.ID = attr.ID) noun.project = record.get('project')
+                console.log("object : ")
+                console.dir(object)
+                save(object,model)
+                console.log('Retrieved', record.get('Name'));
+            });
         });
     });
-    
-})
-
-console.log("nounObjects out of hte scope : " + nounObjects)
-syncer.nounObjects = nounObjects
-
-module.exports = syncer;
+}
---
+++ setup_path.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+const env = process.env;
+STORYTELLER_BASE = env.STORYTELLER_BASE
+
+PROJECT=env.storyteller_project == null?'default':env.storyteller_project
+PROJECT=PROJECT === ''?'default':PROJECT
+
+Conventions = {}
+Conventions.LOCAL_REST_BASE = STORYTELLER_BASE
+PROJECT_BASE = STORYTELLER_BASE + "projects/" + PROJECT +"/"
+NOUN_BASE = PROJECT_BASE + "nouns/"
+VERB_BASE = STORYTELLER_BASE + "verbs/"
+SENTENCE_BASE = PROJECT_BASE + "sentences/"
+
+
+NOUNS.ID = "nouns"
+PROJECTS.ID = "projects"
+ATTRS.ID = "attrs"
+VERBS.ID = "verbs"
+SENTENCES.ID = "sentences"
+
+
+
+console.log("Target Project : "+PROJECT)
+console.log("PROJECT_BASE : "+PROJECT_BASE)
+console.log("NOUN_BASE : " + NOUN_BASE)
+console.log("VERB_BASE:"+VERB_BASE)
+console.log("SENTENCE_BASE:" +SENTENCE_BASE)
+
+
+
+/// # stack
+RESTBASE=env.RESTBASE
+BFF=env.microjs
+LOGIC=env.nodejs
+API_METHOD=env.graphql
+BFF_API_CLIENT=env.axios
+CLIENT_LANG=env.js
+
+
+
+console.log("RESTBASE : "+RESTBASE)
+console.log("BFF : "+BFF)
+console.log("LOGIC : "+LOGIC)
+console.log("API_METHOD : "+API_METHOD)
+console.log("BFF_API_CLIENT : "+BFF_API_CLIENT)
+console.log("CLIENT_LANG : "+CLIENT_LANG)
+
+//defaulting
+RESTBASE=RESTBASE === ''?'local':RESTBASE
+BFF=BFF === ''?'microjs':BFF
+LOGIC=LOGIC === ''?'nodejs':LOGIC
+API_METHOD=API_METHOD === ''?'rest':API_METHOD
+BFF_API_CLIENT=BFF_API_CLIENT === ''?'rest':BFF_API_CLIENT
+CLIENT_LANG=CLIENT_LANG === ''?'js':CLIENT_LANG
+
+
+RESTBASE=RESTBASE === ''?'local':RESTBASE
+RESTBASE=RESTBASE === ''?'local':RESTBASE
+
